5883d388b7af104c746833f48942d39e95ba0c5a
TheMalachite <eliasgheeraert@gmail.com>
2020-11-19 00:06:23 +0100

    bismuth: Paranoid Android bringup

--- device.mk
+++ device.mk
@@ -59,7 +59,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-resurrection
+    $(LOCAL_PATH)/overlay-pa
 
 # Overlays -- replace official
 PRODUCT_PACKAGES += \
